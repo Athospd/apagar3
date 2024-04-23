5e5a26e4957e9bf78c7ed204b2067c774fe533b5
Athos Damiani <Athos.Damiani@bankofengland.co.uk>
2024-04-23 22:27:40 +0100

    first commit

--- R/dev.R
+++ R/dev.R
@@ -1,7 +1,12 @@
+
 usethis::create_package("apagar3")
 usethis::use_git()
 usethis::use_github()
 usethis::use_r("calcs")
+# Modify calcs.R
+usethis::use_version(which = "minor")
+# options(usethis.allow_nested_project = TRUE)
+devtools::build()
 usethis::use_readme_rmd()
 usethis::use_vignette("how-to", title = "How to use")
 usethis::use_pkgdown_github_pages()
