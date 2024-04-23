b2155e1c2a81d87e4e3bdab4f3a6a36b045d380d
Athos Damiani <Athos.Damiani@bankofengland.co.uk>
2024-04-23 22:26:10 +0100

    Increment version number to 0.1.0

---
+++ R/calcs.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' Aasdasda sda sd asda sd asa sd
+#'
+#' @param a adasd asdasd asd
+#' @param b asda sa sa as dasd asd
+#'
+#' @return
+#' @export
+#'
+#' @examples
+foo <- function(a, b) {
+  a + b
+}
---
+++ R/dev.R
@@ -0,0 +1,9 @@
+usethis::create_package("apagar3")
+usethis::use_git()
+usethis::use_github()
+usethis::use_r("calcs")
+usethis::use_readme_rmd()
+usethis::use_vignette("how-to", title = "How to use")
+usethis::use_pkgdown_github_pages()
+# Modify '_pkgdown.yml'
+# commit all and push (first commit)
